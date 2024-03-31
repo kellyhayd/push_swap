603bdbe9adbafd91196c1f3f2984426ed363510a
Kelly Hayd <hayd.kelly@gmail.com>
2024-03-31 18:12:20 -0300

    reorg and include t_data current

--- src/algorithm_10.c
+++ src/algorithm_10.c
@@ -12,22 +12,22 @@
 
 #include "push_swap.h"
 
-void	sort_ten(t_stack **stack_a, t_stack **stack_b)
+void	sort_ten(t_stack **stack_a, t_stack **stack_b, t_data *cur)
 {
 	t_stack	*node_min;
+	t_stack	*tmp_b;
 	int		min;
-	int		size;
 	int		middle;
 	int		direction;
 
-	size = lstsize(*stack_a);
-	while (size > 5)
+	tmp_b = *stack_b;
+	while (cur->size > 5)
 	{
 		min = get_min(stack_a);
 		node_min = *stack_a;
 		while (node_min->num != min)
 			node_min = node_min->next;
-		middle = (size - 1) / 2;
+		middle = cur->size / 2;
 		if (node_min->idx >= middle)
 			direction = 1;
 		else
@@ -36,13 +36,13 @@ void	sort_ten(t_stack **stack_a, t_stack **stack_b)
 		{
 			if (direction == 1)
 				rra(stack_a);
-			else
+			else if (direction == 2)
 				ra(stack_a);
 		}
 		pb(stack_a, stack_b);
-		size = lstsize(*stack_a);
+		cur->size = lstsize(*stack_a);
 	}
-	sort_five(stack_a, stack_b);
-	while (*stack_b)
+	sort_five(stack_a, stack_b, cur);
+	while (*stack_b != tmp_b)
 		pa(stack_a, stack_b);
 }
