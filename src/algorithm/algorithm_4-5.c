85482b11422e57bca31d4d50f1f07e9616e33a96
Kelly Rocha Hayd <krocha-h@c1r3p2.42sp.org.br>
2024-04-01 11:20:51 -0300

    norm algo 4-5

--- src/algorithm/algorithm_4-5.c
+++ src/algorithm/algorithm_4-5.c
@@ -6,7 +6,7 @@
 /*   By: krocha-h <krocha-h@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/25 15:58:23 by krocha-h          #+#    #+#             */
-/*   Updated: 2024/04/01 11:14:35 by krocha-h         ###   ########.fr       */
+/*   Updated: 2024/04/01 11:19:33 by krocha-h         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,6 +39,26 @@ void	sort_four(t_stack **stack_a, t_stack **stack_b, t_data *data)
 		pa(stack_a, stack_b);
 }
 
+void	move_five(t_stack **stack_a, t_stack **stack_b, int min, int needs_pa)
+{
+	if ((*stack_a)->next->num == min)
+		sa(stack_a);
+	else if ((*stack_a)->next->next->num == min)
+	{
+		ra(stack_a);
+		ra(stack_a);
+	}
+	else if ((*stack_a)->next->next->next->num == min)
+	{
+		rra(stack_a);
+		rra(stack_a);
+	}
+	else if ((*stack_a)->next->next->next->next->num == min)
+		rra(stack_a);
+	pb(stack_a, stack_b);
+	needs_pa = 1;
+}
+
 void	sort_five(t_stack **stack_a, t_stack **stack_b, t_data *data)
 {
 	int	min;
@@ -47,24 +67,7 @@ void	sort_five(t_stack **stack_a, t_stack **stack_b, t_data *data)
 	min = get_min(stack_a);
 	needs_pa = 0;
 	if (data->size_now == 5)
-	{
-		if ((*stack_a)->next->num == min)
-			sa(stack_a);
-		else if ((*stack_a)->next->next->num == min)
-		{
-			ra(stack_a);
-			ra(stack_a);
-		}
-		else if ((*stack_a)->next->next->next->num == min)
-		{
-			rra(stack_a);
-			rra(stack_a);
-		}
-		else if ((*stack_a)->next->next->next->next->num == min)
-			rra(stack_a);
-		pb(stack_a, stack_b);
-		needs_pa = 1;
-	}
+		move_five(stack_a, stack_b, min, needs_pa);
 	data->size_now = lstsize(*stack_a);
 	sort_four(stack_a, stack_b, data);
 	if (needs_pa)
